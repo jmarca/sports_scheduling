4f7813001d91dcf8f664a75286a9b93590e5fc27
James E. Marca <james@activimetrics.com>
2019-08-08 17:16:16 -0700

    big ugly commit
    previous idea didn't pan out, was slower (forcing home away to be
    approx equal numbers, then only checking home side of pool play)  So
    stripped that back out.
    
    Then implemented some command line flags.  Not actually being used yet.

--- src/cp_sat/field_schedule_redo.py
+++ src/cp_sat/field_schedule_redo.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import numpy as np
+import argparse
 
 from ortools.sat.python import cp_model
 
@@ -9,7 +8,13 @@ from ortools.sat.python import cp_model
 def model(num_teams=32,
           num_matchdays=10,
           num_matches_per_day=16,
-          num_groups=4):
+          num_pools=4,
+          max_home_stand=2,
+          timelimit=None,
+          cpu=None,
+          csv=None,
+          debug=None
+):
 
     model = cp_model.CpModel()
 
@@ -18,13 +23,13 @@ def model(num_teams=32,
     if num_matchdays % 2:
         home_hard_constraint = False
 
-    groups = []
-    group_size = int(num_teams//num_groups)
-    for g in range(num_groups):
-        if g < num_groups-1:
-            groups.append(list(range(int(g*group_size),int((g+1)*group_size))))
+    pools = []
+    pool_size = int(num_teams//num_pools)
+    for g in range(num_pools):
+        if g < num_pools-1:
+            pools.append(list(range(int(g*pool_size),int((g+1)*pool_size))))
         else:
-            groups.append(list(range(int(g*group_size),num_teams)))
+            pools.append(list(range(int(g*pool_size),num_teams)))
 
     matchdays = range(num_matchdays)
     matches = range(num_matches_per_day)
@@ -62,7 +67,7 @@ def model(num_teams=32,
         at_home.append([])
         for i in teams:
             fixtures[d].append([])
-            home_pool = num_groups
+            home_pool = num_pools
             for j in teams:
                 fixtures[d][i].append(model.NewBoolVar('fixture: home team %i, opponent %i, matchday %i' % (i,j,d)))
                 if i == j:
@@ -70,61 +75,63 @@ def model(num_teams=32,
             # is team i playing at home on day d?
             at_home[d].append(model.NewBoolVar('team %i is home on matchday %i' % (i,d)))
 
-    # balance home and away games
-    for t in teams:
-        all_home_games.append([])
-        for d in matchdays:
-            all_home_games[t] += fixtures[d][t]
-        if home_hard_constraint:
-            model.Add(sum(all_home_games) == num_homegames)
-        else:
-            # odd number of games, so might have one more or one less homegame
-            model.Add(sum(all_home_games) >= num_homegames)
-            model.Add(sum(all_home_games) <= num_homegames+1)
+    # balance home and away games?  I think this is redundant with the
+    # "breaks" constraints, later.
+    # for t in teams:
+    #     all_home_games.append([])
+    #     for d in matchdays:
+    #         all_home_games[t] += fixtures[d][t]
+
+    #     if home_hard_constraint:
+    #         model.Add(sum(all_home_games[t]) == num_homegames)
+    #     else:
+    #         # odd number of games, so might have one more or one less homegame
+    #         model.Add(sum(all_home_games[t]) >= num_homegames)
+    #         model.Add(sum(all_home_games[t]) <= num_homegames+1)
 
     # pool play loop
-    # home team group is outer loop
+    # home team pool is outer loop
 
     # prep the arrays
     for t in teams:
         pool_play.append([])
-        for ppi in range(num_groups):
+        for ppi in range(num_pools):
             pool_play[t].append([])
 
-    for ppi in range(num_groups):
+    for ppi in range(num_pools):
         pool_balance.append([])
-        for ppj in range(num_groups):
+        for ppj in range(num_pools):
             pool_balance[ppi].append([])
 
 
-    for ppi in range(num_groups):
-        for t in groups[ppi]:
-            # other team group is inner loop
-            for ppj in range(num_groups):
+    for ppi in range(num_pools):
+        for t in pools[ppi]:
+            # other team pool is inner loop
+            for ppj in range(num_pools):
                 # over all the days, have to play each pool at least once
                 for d in matchdays:
-                    for opponent in groups[ppj]:
+                    for opponent in pools[ppj]:
                         if t == opponent:
                             # cannot play self
                             continue
                         # save case of t is home, playing vs pool j
                         pool_play[t][ppj].append(fixtures[d][t][opponent])
                         # save case of t is away, playing vs pool j
-                        ## pool_play[t][ppj].append(fixtures[d][opponent][t])
+                        pool_play[t][ppj].append(fixtures[d][opponent][t])
                         # save pool home vs pool away case
                         pool_balance[ppi][ppj].append(fixtures[d][t][opponent])
 
     # pulling this out of the above loop for safety
     for t in teams:
-        for ppi in range(num_groups):
+        for ppi in range(num_pools):
             # over all the days, have to play each pool at least once
             # model.AddBoolOr(pool_play[t][ppj])
             # in order to require more than one, use Add(sum(...))
-            model.Add(sum(pool_play[t][ppi]) >= 1)
-    # now for group to group, balance play
+            model.Add(sum(pool_play[t][ppi]) >= 2)
+    # now for pool to pool, balance play
     # 10 is hardcoded for now
-    for ppi in range(num_groups):
-        for ppj in range(num_groups):
+    for ppi in range(num_pools):
+        for ppj in range(num_pools):
             model.Add(sum(pool_balance[ppi][ppj]) == 10)
 
     # for this loop list possible opponents
@@ -180,39 +187,51 @@ def model(num_teams=32,
 
     # forbid sequence of 3 homes or 3 aways in a row
     for t in teams:
-        for d in range(num_matchdays - 2):
-            model.AddBoolOr([at_home[d][t],
-                             at_home[d+1][t],
-                             at_home[d+2][t]])
-            model.AddBoolOr([at_home[d][t].Not(),
-                             at_home[d+1][t].Not(),
-                             at_home[d+2][t].Not()])
-
-
+        for d in range(num_matchdays - max_home_stand):
+            model.AddBoolOr([at_home[d+offset][t] for offset in range(max_home_stand+1)])
+            model.AddBoolOr([at_home[d+offset][t].Not() for offset in range(max_home_stand+1)])
+            # note, this works because AddBoolOr means at least one
+            # element must be true.  if it was just AddBoolOr([home0,
+            # home1, ..., homeN]), then that would mean that one or
+            # all of these could be true, and you could have an
+            # infinite sequence of home games.  However, that home
+            # constraint is matched with an away constraint.  So the
+            # combination says:
+            #
+            # AddBoolOr([home0, ... homeN]) at least one of these is true
+            # AddBoolOr([away0, ... awayN]) at least one of these is true
+            #
+            # taken together, at least one home from 0 to N is true,
+            # which means at least one away0 to awayN is false.  At
+            # the same time, at least one away is true, which means
+            # that the corresponding home is false.  So together, this
+            # prevents a sequence of one more than max_home_stand to
+            # take place.
 
     # objective using breaks concept
     breaks = []
     for t in teams:
         for d in range(num_matchdays-1):
             breaks.append(model.NewBoolVar('two home or two away for team %i, starting on matchday %i' % (t,d)))
-            # not sure what the point of these constraints are
 
-            # at least one of at_home[d][t], at_home[d+1][t], or breaks[-1] is true
-            # if break is false, then one or both of [home d, home d+1] must be true
-            # if break is true, then one of both of [home d, home d+1] can be false
             model.AddBoolOr([at_home[d][t],at_home[d+1][t],breaks[-1]])
-            #
-            # assuming "away" means at_home[][].Not(), then
-            #
-            # at least one of away[d][t], away[d+1][t], or breaks[-1] is true
-            # if break is false, then one or both of [away d, away d+1] must be true
-            # if break is true, then one of both of [away d, away d+1] can be false
             model.AddBoolOr([at_home[d][t].Not(),at_home[d+1][t].Not(),breaks[-1]])
-            # if break is false, then at least one of
+
             model.AddBoolOr([at_home[d][t].Not(),at_home[d+1][t],breaks[-1].Not()])
             model.AddBoolOr([at_home[d][t],at_home[d+1][t].Not(),breaks[-1].Not()])
+
+            # I couldn't figure this out, so I wrote a little program
+            # and proved it.  These effectively are identical to
+            #
+            # model.Add(at_home[d][t] == at_home[d+1][t]).OnlyEnforceIf(breaks[-1])
+            # model.Add(at_home[d][t] != at_home[d+1][t]).OnlyEnforceIf(breaks[-1].Not())
+            #
+            # except they are a little more efficient, I believe.  Wrote it up in a blog post
+
+
+
     # constrain breaks
-    model.Add(sum(breaks) >= 2*num_teams - 4)
+    model.Add(sum(breaks) >= num_matchdays)
     model.Minimize(sum(breaks))
     # run the solver
     solver = cp_model.CpSolver()
@@ -238,12 +257,12 @@ def model(num_teams=32,
     pool_vs_pool = []
     # these should be balanced
     team_pool_play=[]
-    for i in range(num_groups):
-        pool_vs_pool.append([0 for j in range(num_groups)])
-        for t in groups[i]:
+    for i in range(num_pools):
+        pool_vs_pool.append([0 for j in range(num_pools)])
+        for t in pools[i]:
             team_pool_play.append([])
-            for j in range(num_groups):
-                for other in groups[j]:
+            for j in range(num_pools):
+                for other in pools[j]:
                     team_pool_play[t].append(0)
 
 
@@ -258,31 +277,75 @@ def model(num_teams=32,
                     print('day %i game %i home %i away %i' %(d+1,game,t+1,other+1))
 
     for d in matchdays:
-        for i in range(num_groups):
-            for t in groups[i]:
-                for j in range(num_groups):
-                    for other in groups[j]:
+        for i in range(num_pools):
+            for t in pools[i]:
+                for j in range(num_pools):
+                    for other in pools[j]:
                         home = solver.Value(fixtures[d][t][other])
                         away = solver.Value(fixtures[d][other][t])
                         if home:
                             team_pool_play[t][j] += 1
-                            pools[i][j] += 1
+                            pool_vs_pool[i][j] += 1
                         if away:
                             # t is away, but still playing vs pool j
                             team_pool_play[t][j] += 1
 
 
     all_combinations_sum = 0
-    for i in range(num_groups):
-        for j in range(num_groups):
-            print('pool % at home vs pool % away, count = %'%(i,j,pool_vs_pool[i][j]))
+    for i in range(num_pools):
+        for j in range(num_pools):
+            print('pool %i at home vs pool %i away, count = %i'%(i,j,pool_vs_pool[i][j]))
             all_combinations_sum += pool_vs_pool[i][j]
     print('all combinations sum to',all_combinations_sum)
 
-    for i in range(num_groups):
-        for t in groups[i]:
-            for j in range(num_groups):
+    for i in range(num_pools):
+        for t in pools[i]:
+            for j in range(num_pools):
                 print('team %i versus pool %i = %i' % (t+1,j,team_pool_play[t][j]))
 
-if __name__ == '__main__':
+    # print break results, to get a clue what they are doing
+    print('Breaks')
+    for b in breaks:
+        print('  %s is %i' % (b.Name(), solver.Value(b)))
+
+def main():
+    """Entry point of the program."""
+    parser = argparse.ArgumentParser(description='Solve sports league match play assignment problem')
+    parser.add_argument('-t,--teams', type=int, dest='num_teams',
+                        help='Number of teams in the league')
+
+    parser.add_argument('-d,--days', type=int, dest='num_matchdays',
+                        help='Number of days on which matches are played.  Default is enough days such that every team can play every other team, or (number of teams - 1)')
+
+    parser.add_argument('--matches_per_day', type=int, dest='num_matches_per_day',
+                        help='Number of matches played per day.  Default is number of teams divided by 2.  If greater than the number of teams, then this implies some teams will play each other more than once.  In that case, home and away should alternate between the teams in repeated matchups.')
+
+    parser.add_argument('-p,--pools', type=int, dest='num_pools',default=4,
+                        help='CSV file for dumping output for demand details (including invalid demands, etc)')
+
+    parser.add_argument('--csv', type=str, dest='csv_file', default='output.csv',
+                        help='A file to dump the team assignments.  Default is output.csv')
+
+    parser.add_argument('--timelimit', type=int, dest='timelimit', default=10,
+                        help='Maximum run time for solver, in minutes.  Default is 10 minutes.')
+    parser.add_argument('--cpu',type=int,dest='cpu',default=6,
+                        help='Number of CPUs to use for solver.  Default is 6')
+
+    parser.add_argument('--debug', type=bool, dest='debug', default=False,
+                        help="Turn on some print statements.")
+
+    parser.add_argument('--max_home_stand',type=int,dest='max_home_stand',default=2,
+                        help="Maximum consecutive home or away games.  Default to 2, which means three home or away games in a row is forbidden.")
+
+    args = parser.parse_args()
+
+    # set default for num_matchdays
+    num_matches_per_day = args.num_matches_per_day
+    if not num_matches_per_day:
+        num_matches_per_day = args.num_teams - 1
+
+
     model()
+
+if __name__ == '__main__':
+    main()
