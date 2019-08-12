c12c27402ac376523478744ef6a07adfac12e75c
James E. Marca <james@activimetrics.com>
2019-08-12 13:03:34 -0700

    add google copyright apache license

--- src/cp_sat/sports_schedule_sat.py
+++ src/cp_sat/sports_schedule_sat.py
@@ -1,11 +1,32 @@
+# based on sports_schedulting_sat.cc, Copyright 2010-2018 Google LLC
+#
+# translated to Python by James E. Marca, with the addition of some
+# command line options, inter-pool play constraints, and CSV output dump
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Essentially a translation of sports_scheduling_sat.cc from the C++ example
+
+Added CSV output option, and pool vs pool type constraints
+
+"""
 import argparse
 import os
 import re
 import csv
 
 from ortools.sat.python import cp_model
-
-# Essentially a translation of sports_scheduling_sat.cc from the C++ example
 
 def csv_dump_results(solver,fixtures,pools,num_teams,num_matchdays,csv_basename):
     matchdays = range(num_matchdays)
