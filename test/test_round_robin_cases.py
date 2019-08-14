c421b3e47cdf0aad0d6b1d4476f86a2ffb0db6f5
James E. Marca <james@activimetrics.com>
2019-08-13 21:44:33 -0700

    more splitting

---
+++ test/test_round_robin_cases.py
@@ -0,0 +1,63 @@
+# hack to capture stdout to a string, to test it
+import re
+import os
+import subprocess
+import io
+import sys
+from contextlib import contextmanager
+import filecmp
+
+
+def test_rr_cases():
+
+    # now for various combinations of inputs
+    output_file = 'test_output.csv'
+
+    # test exact round robin case, but just once
+    process_command_line = ['python','src/sports_schedule_sat.py'
+                            ,'-t','8'
+                            ,'-d','7'
+                            ,'-p','2'
+                            ,'--cpu','2'
+                            ,'--debug'
+                            ,'--timelimit','10'
+                            ,'--csv',output_file]
+    try:
+        proc = subprocess.run(process_command_line, encoding='utf8', capture_output=True)
+        out = proc.stdout
+        err = proc.stderr
+        print('out 186 is ',out)
+        assert re.search('FEASIBLE', out, re.MULTILINE)
+        assert re.search('num_search_workers: 2',err,re.MULTILINE)
+    except:
+        assert False
+
+    try:
+        # clean up the temp file
+        os.unlink(output_file)
+    except:
+        print('no file to delete')
+
+    # test exact round robin case, but just twice around
+    process_command_line = ['python','src/sports_schedule_sat.py'
+                            ,'-t','8'
+                            ,'-d','14'
+                            ,'-p','2'
+                            ,'--cpu','2'
+                            ,'--debug'
+                            ,'--timelimit','10'
+                            ,'--csv',output_file]
+    try:
+        proc = subprocess.run(process_command_line, encoding='utf8', capture_output=True)
+        out = proc.stdout
+        err = proc.stderr
+        assert re.search('OPTIMAL', out, re.MULTILINE)
+        assert re.search('num_search_workers: 2',err,re.MULTILINE)
+    except:
+        assert False
+
+    try:
+        # clean up the temp file
+        os.unlink(output_file)
+    except:
+        print('no file to delete')
